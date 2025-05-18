d4df8630da2cd75654a9c696931dcc62565c05d0
Charles Kamiri <charleskamiri6@gmail.com>
2025-05-18 05:36:28 +0300

    feat: Initialize db connection

---
+++ src/db/schema.ts
@@ -0,0 +1,49 @@
+import {
+	pgTable,
+	text,
+	uuid,
+	integer,
+	boolean,
+	timestamp,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const files = pgTable("files", {
+	id: uuid("id").defaultRandom().primaryKey(),
+
+	// basic file/folder info
+	name: text("name").notNull(),
+	path: text("path").notNull(),
+	size: integer("size").notNull(),
+	type: text("type").notNull(), // folder/file
+
+	// storage info
+	fileUrl: text("file_url").notNull(),
+	thumbnailUrl: text("thumbnail_url"),
+
+	// Ownership info
+	userId: text("user_id").notNull(),
+	parentId: uuid("parent_id"),
+
+	// file folder flags
+	isFolder: boolean("is_folder").default(false).notNull(),
+	isStarred: boolean("is_starred").default(false).notNull(),
+	isTrash: boolean("is_trash").default(false).notNull(),
+
+	// timestamps
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+	parent: one(files, {
+		fields: [files.parentId],
+		references: [files.id],
+	}),
+
+	children: many(files),
+}));
+
+// Type defs
+export type File = typeof files.$inferSelect;
+export type NewFile = typeof files.$inferInsert;
